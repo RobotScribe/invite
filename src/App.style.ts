461c4e46b666cd21c08143f62cf6e181750ed4d3
RobotScribe <quentinso@theodo.fr>
2021-03-15 22:06:18 +0100

    Add prettier

--- src/App.style.ts
+++ src/App.style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const AppContainer = styled.div`
   display: flex;
@@ -7,4 +7,4 @@ export const AppContainer = styled.div`
   align-items: center;
   justify-content: center;
 `;
-AppContainer.displayName = 'AppContainer';
+AppContainer.displayName = "AppContainer";
--- src/index.style.ts
+++ src/index.style.ts
@@ -1,11 +1,11 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
-    colors: {
-        blue: {
-            300: "#2C54EA",
-            500: "#272D45",
-            900: "#202437",
-        }
-    }
+  colors: {
+    blue: {
+      300: "#2C54EA",
+      500: "#272D45",
+      900: "#202437",
+    },
+  },
 });
