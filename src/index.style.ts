0930065612010f49fe2a59fe43af1d3f4d4e500b
RobotScribe <quentinso@theodo.fr>
2021-03-15 21:05:14 +0100

    :sparkles: Add chakra ui

---
+++ src/index.style.ts
@@ -0,0 +1,7 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+    colors: {
+        lightBlue: "#2C54EA",
+    }
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from "@chakra-ui/react"
+
 import './index.css';
 import App from './App';
+import { theme } from './index.style';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
