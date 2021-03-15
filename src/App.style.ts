d3e7c43f27630d38e689c1091097be5699d7f73a
RobotScribe <quentinso@theodo.fr>
2021-03-15 20:28:36 +0100

    :fire: Clean files

---
+++ src/App.style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+AppContainer.displayName = 'AppContainer';
